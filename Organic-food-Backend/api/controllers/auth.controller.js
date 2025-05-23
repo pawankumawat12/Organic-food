82a4a595bdc86feadc10944a0f2048f03c999d7e
ashokkumawat-autovyn <ashok.kumar@autovyn.in>
2025-05-23 11:12:14 +0530

    register user api

--- Organic-food-Backend/api/controllers/auth.controller.js
+++ Organic-food-Backend/api/controllers/auth.controller.js
@@ -1,9 +1,10 @@
-import { register } from '../service/auth.service.js';
+import { register } from '../services/auth.service.js';
 import logger from '../../utils/logger.js';
 
 export const registerUser = async (req, res, next) => {
   try {
     const result = await register(req.body);
+    console.log('req.body', req.body);
     res.status(201).json(result);
   } catch (error) {
     logger.error('Register error:', error);
--- Organic-food-Backend/api/routes/auth.routes.js
+++ Organic-food-Backend/api/routes/auth.routes.js
@@ -1,21 +1,9 @@
-// import express from 'express';
-// import { registerUser, loginUser, registerUser, signUpAdminService } from '../controllers/auth.controller.js';
-// import { validateBody } from '../../middlewares/validate.middleware.js';
-// import { registerSchema, loginSchema } from '../validators/auth.validator.js';
+    import express from 'express';
+    import { registerUser } from '../controllers/auth.controller.js';
+    import { validateBody } from '../../middlewares/validate.middleware.js';
+    import { registerSchema } from '../validators/auth.validator.js';
 
-// const router = express.Router();
+    const router = express.Router();
+    router.post('/register', validateBody(registerSchema), registerUser);
 
-// router.post('/registerUser',  validateBody(registerSchema), registerUser);
-// router.post('/signUpAdminService',  validateBody(registerSchema), signUpAdminService);
-
-// export default router;
-
-import express from 'express';
-import { registerUser } from '../controller/auth.controller.js';
-import { validateBody } from '../../middleware/validate.middleware.js';
-import { registerSchema } from '../validator/auth.validator.js';
-
-const router = express.Router();
-router.post('/register', validateBody(registerSchema), registerUser);
-
-export default router;
+    export default router;
