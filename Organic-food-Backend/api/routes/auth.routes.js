e3ed191d5a62ddbfe33fb61ad06cf13be85efb92
ashokkumawat-autovyn <ashok.kumar@autovyn.in>
2025-05-23 12:03:46 +0530

    new api changes

--- Organic-food-Backend/api/routes/auth.routes.js
+++ Organic-food-Backend/api/routes/auth.routes.js
@@ -2,11 +2,6 @@
     import { registerUser } from '../controllers/auth.controller.js';
     import { validateBody } from '../../middlewares/validate.middleware.js';
     import { registerSchema } from '../validators/auth.validator.js';
-    import express from 'express';
-    import { registerUser } from '../controllers/auth.controller.js';
-    import { validateBody } from '../../middlewares/validate.middleware.js';
-    import { registerSchema } from '../validators/auth.validator.js';
-
     const router = express.Router();
     router.post('/Signup', validateBody(registerSchema), registerUser);
 
