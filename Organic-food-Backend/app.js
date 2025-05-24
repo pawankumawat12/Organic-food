bcc3dcebcbdfa699bcb93ac26d0ce8b13ce11166
ashokkumawat-autovyn <ashok.kumar@autovyn.in>
2025-05-24 10:28:14 +0530

    ipsetformobile

--- Organic-food-Backend/app.js
+++ Organic-food-Backend/app.js
@@ -19,10 +19,12 @@ const __dirname = path.dirname(__filename);
 const swaggerFilePath = path.join(__dirname, 'swagger', 'swagger-output.json');
 const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, 'utf-8'));
 const corsOptions = {
-  origin: ['http://localhost:5173', 
-  'https://assuring-javelin-smoothly.ngrok-free.app'
+  origin: ['http://localhost:5173',
+    'https://assuring-javelin-smoothly.ngrok-free.app',
+    'http://152.58.70.211:5173'
+
   ],
-  optionsSuccessStatus: 200 
+  optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
 app.use(express.json());
